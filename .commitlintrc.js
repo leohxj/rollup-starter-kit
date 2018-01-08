552d56dc2b74e5890ae0f5d037a50e13a3951a38
Leo Hui <leohxj@gmail.com>
2018-01-08 16:58:16 +0800

    test commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,16 @@
 'use strict';
+var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = {
-  extends: ['cz'],
-  rules: {
-  },
-};
+    "extends" : ['cz'],
+    "rules": {
+        "type-enum": [
+            2,
+            "always",
+            Object.keys(conventionalCommitTypes.types)
+        ],
+        "subject-case": [
+            0
+        ]
+    }
+}
