3d25491f7538185648dfd403b821d2fbba7f66c4
Leo Hui <leohxj@gmail.com>
2018-01-08 19:31:45 +0800

    feat: update cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -5,39 +5,39 @@ module.exports = {
   types: [
     {
       value: 'WIP',
-      name : '💪  WIP: Work in progress'
+      name : '💪  WIP:      Work in progress'
     },
     {
       value: 'feat',
-      name : '✨  Feature: A new feature'
+      name : '✨  feat:     A new feature'
     },
     {
       value: 'fix',
-      name : '🐞  Bug Fixes: A bug fix'
+      name : '🐞  fix:      A bug fix'
     },
     {
       value: 'refactor',
-      name : '🛠  Code Refactoring: A code change that neither fixes a bug nor adds a feature'
+      name : '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
     },
     {
       value: 'docs',
-      name : '📚  Documentation: Documentation only changes'
+      name : '📚  docs:     Documentation only changes'
     },
     {
       value: 'test',
-      name : '🏁  Tests: Adding missing tests or correcting existing tests'
+      name : '🏁  test:     Add missing tests or correcting existing tests'
     },
     {
       value: 'chore',
-      name : '🗯  Chores: Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
+      name : '🗯  chore:    Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
     },
     {
       value: 'style',
-      name : '💅  Style: Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+      name : '💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
     },
     {
       value: 'revert',
-      name : '⏪  revert: Revert to a commit'
+      name : '⏪  revert:   Revert to a commit'
     }
   ],
 
