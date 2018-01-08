7a69152f555f0a4c7b1cdcece7227977f5323c08
Leo Hui <leohxj@gmail.com>
2018-01-08 19:30:59 +0800

    feat: update cz-config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: ['cz'],
+  extends: [
+    'cz'
+  ],
   rules: {
-    'type-enum': [2, 'always', ['feat']]
   }
 };
--- .cz-config.js
+++ .cz-config.js
@@ -4,52 +4,45 @@ module.exports = {
 
   types: [
     {
-      value: '💪  WIP',
+      value: 'WIP',
       name : '💪  WIP: Work in progress'
     },
     {
-      value: '✨  feat',
+      value: 'feat',
       name : '✨  Feature: A new feature'
     },
     {
-      value: '🐞  fix',
+      value: 'fix',
       name : '🐞  Bug Fixes: A bug fix'
     },
     {
-      value: '✏️  chore',
-      name : '✏️  Chores: Other changes that don\'t modify src or test files'
-    },
-    {
-      value: '🎨  style',
-      name : '🎨  Style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
-    },
-    {
-      value: '📚  docs',
-      name : '📚  Documentation: Documentation only changes'
-    },
-    {
-      value: '🛠  refactor',
+      value: 'refactor',
       name : '🛠  Code Refactoring: A code change that neither fixes a bug nor adds a feature'
     },
     {
-      value: '🚀  perf',
-      name : '🚀  Performance Improvements: A code change that improves performance'
+      value: 'docs',
+      name : '📚  Documentation: Documentation only changes'
     },
     {
-      value: '🏁  test',
+      value: 'test',
       name : '🏁  Tests: Adding missing tests or correcting existing tests'
     },
     {
-      value: '🏗️  build',
-      name : '🏗️  Builds: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'
+      value: 'chore',
+      name : '🗯  Chores: Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
     },
     {
-      value: '⏪  revert',
+      value: 'style',
+      name : '💅  Style: Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'revert',
       name : '⏪  revert: Revert to a commit'
     }
   ],
 
   scopes: [],
 
-  allowCustomScopes: true
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"]
 };
