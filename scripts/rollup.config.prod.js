86827e3b9197743d00bed2c8a6cf53016d161a50
Leo Hui <leohxj@gmail.com>
2018-03-04 18:48:37 +0800

    feat: update year

--- scripts/rollup.config.prod.js
+++ scripts/rollup.config.prod.js
@@ -8,7 +8,7 @@ import { name, version, author } from '../package.json';
 // banner
 const banner =
   `${'/*!\n' + ' * '}${name}.js v${version}\n` +
-  ` * (c) 2017-${new Date().getFullYear()} ${author}\n` +
+  ` * (c) 2018-${new Date().getFullYear()} ${author}\n` +
   ` * Released under the MIT License.\n` +
   ` */`;
 
